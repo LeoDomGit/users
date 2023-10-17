6523c7d6ee7584dfb7c81678bc7e6e989126a8ad
LeoDomGit <leodomsolar@gmail.com>
2023-10-17 08:39:32 +0700

    C

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
 import ChiTiet from "./pages/ChiTiet";
+import NotFound from "./pages/NotFound";
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home/>}/>
-          <Route path=":id" element={<Detail/>}/>
           <Route path="/chitiet/:id" element={<ChiTiet/>}/>
+          <Route path="/:id" element={<Detail/>}/>
+          <Route path="*" element={<NotFound/>}/>
+
         </Routes>
       </BrowserRouter>
     </>
